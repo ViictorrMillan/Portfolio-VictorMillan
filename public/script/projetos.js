c30425b9af767e315ff115bd22f3e7f71319bdd5
Victor <victorhugomartinsmillan@hotmail.com>
2025-02-25 22:19:17 -0300

    feat: Adiciona os icons do header, Adicao de arquivos para outras secoes

---
+++ public/script/projetos.js
@@ -0,0 +1,27 @@
+// Em projetos.js
+export const projetos = [
+    {
+        nome: "Jogo do Número Secreto",
+        imagem: "/images/numerosecreto.png",
+        descricao: "Este projeto é um jogo simples onde o jogador tenta adivinhar um número secreto gerado pelo computador. Foi desenvolvido como parte do curso One utilizando JavaScript.",
+        link: "https://numerosecreto-one-murex.vercel.app"
+    },
+    {
+        nome: "Decodificador",
+        imagem: "/images/decodificador.png",
+        descricao: "Este projeto foi desenvolvido como parte de um desafio do curso ONE para criar uma aplicação de criptografia de textos. A ideia é que você possa trocar mensagens secretas com outras pessoas utilizando um sistema de criptografia baseado em substituição de letras.",
+        link: "https://decodificador-one-pi.vercel.app"
+    },
+    {
+        nome: "Controlador de Temperatura",
+        imagem: "/images/IOT.png",
+        descricao: "O controlador tem a função de monitorar a temperatura ambiente por meio de um sensor, exibindo-a em um display LCD e controlando o funcionamento de um sistema de resfriamento e de uma máquina de acordo com os limites de temperatura definidos. O sistema aciona o resfriamento quando a temperatura ultrapassa um certo limite e desliga a máquina se a temperatura atingir um nível crítico, ligando-a novamente quando a temperatura baixar.",
+        link: "https://www.tinkercad.com/things/jEgVb9KmkKy-controlador-de-temperatura"
+    },
+    {
+        nome: "Museu Nacional - UFRJ",
+        imagem: "/images/Museu.png",
+        descricao: "Projeto feito para o curso da Udemy Desenvolvimento Web Full Stack com Python e Django.: Utilizando as linguagens HTML e CSS para estruturar um site baseado no Museu Nacional - UFRJ",
+        link: "https://projeto-museu-nacional-ufrj.vercel.app"
+    }
+];
